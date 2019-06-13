32f34c985b3a9f15ac19e0718c4ef96f881cd4af
Carlos <chassismartins@gmail.com>
2019-06-13 20:04:07 -0300

    multiplicacao

--- Projeto.c
+++ Projeto.c
@@ -10,9 +10,18 @@ int subtracao(int a, int b)
 	int sub_num = a- b;
 	return sub_num;
 }
+
+int mult(int a, int b)
+{
+	int mult_num = a*b;
+	return mult_num;
+}
+
+
 int main(){
 	
 	printf("soma:%d", soma(5,5));
 	printf("\nsubtracao:%d", subtracao(15,5));
+	printf("\nmultiplicacao:%d", mult(15,5));
 	return 0;
 }
