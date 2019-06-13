bc5389177f8600c57f1c7934e33681f8876ec210
Carlos <chassismartins@gmail.com>
2019-06-13 19:57:57 -0300

    adicionando funcao de subtracao

--- Projeto.c
+++ Projeto.c
@@ -5,9 +5,14 @@ int soma(int a, int b)
 	int soma_num = a+ b;
 	return soma_num;
 }
-
+int subtracao(int a, int b)
+{
+	int sub_num = a- b;
+	return sub_num;
+}
 int main(){
 	
 	printf("soma:%d", soma(5,5));
+	printf("\nsubtracao:%d", subtracao(15,5));
 	return 0;
 }
